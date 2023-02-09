2c4c3a3de5555f91bf63a7cab1cfa45a74326960
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-09 17:49:23 +0100

    scaffolding for ph and condensation wrappers

--- coal_mapper/topology.py
+++ coal_mapper/topology.py
@@ -1,1 +1,19 @@
 # TODO: Implement Persistence Homology based statistics here
+
+
+class PersistentHomology:
+    """Handle Communications with Ripser and Persim"""
+
+    # TODO:
+    # generate persistence diagrams and plot
+    # Feature selection
+    # save diagrams
+
+
+class Condensation:
+    """Handle Communications with PECAN"""
+
+    # TODO:
+    # check whether PECAN is installed
+    # feature selection and convert to np.array, save as txt
+    # Specify Callbacks
