42ca367bef54b1d5850eeb495742354e1a5cc5a6
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-25 16:43:30 +0100

    renaming

--- coal_mapper/condensation_analysis.py
+++
@@ -1,7 +0,0 @@
-"""Run Analysis using scripts from the PECAN Library: https://github.com/KrishnaswamyLab/PECAN"""
-
-from pecan.functor import DiffusionCondensation
-
-
-diffusion_condensation = DiffusionCondensation(callbacks=callbacks, kernel_fn=kernel_fn)
-data = diffusion_condensation(X, epsilon=0.4)
