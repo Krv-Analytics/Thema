af343082a77d7c2b713b04baf78ed843fe484966
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-25 16:19:14 +0100

    removing unecessary callbacks

--- coal_mapper/pecan/condensation.py
+++ coal_mapper/pecan/condensation.py
@@ -10,7 +10,7 @@ import numpy as np
 
 import data
 
-from callbacks import CalculateBifiltration
+
 from callbacks import CalculateDiffusionHomology
 from callbacks import CalculatePersistentHomology
 
