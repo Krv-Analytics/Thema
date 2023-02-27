84eff563cf8ea401835e71fb815ab8f379a139d7
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-27 19:29:04 +0100

    checks for empty graphs

--- coal_mapper/utils.py
+++ coal_mapper/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 from sklearn.cluster import KMeans
+import pandas as pd
 
 
 from nammu.topology import calculate_persistence_diagrams
@@ -71,7 +72,7 @@ class MapperTopology:
     def set_graph(
         self,
         cover,
-        clusterer=KMeans(5, n_init="auto", random_state=1618033),
+        clusterer=KMeans(5, n_init="auto", random_state=2023),
         min_intersection: int = 1,
     ):
         """Generate a new networkX graph from Data via mapper. Recompute"""
@@ -86,21 +87,23 @@ class MapperTopology:
                 min_intersection = 1
             else:
                 print("Computing Mapper Algorithm...")
-                if self._mapper is None:
+                # Save for looping over min_intersection
+                if self._mapper is not None:
+                    self._graph = self._mapper.to_networkx(min_intersection)
+                else:
                     self._mapper = CoalMapper(X=self.data)
-
-                self._mapper.clusterer = clusterer
-
-                self._mapper.compute_mapper(n_cubes, perc_overlap)
+                    self._mapper.clusterer = clusterer
+                    self._mapper.compute_mapper(n_cubes, perc_overlap)
                 print("Generating networkx Graph...")
                 self._graph = self._mapper.to_networkx(min_intersection)
 
-                # Automatically Compute OR Curvature and corresponding Diagrams when changing a graph
-                print(
-                    "Using Ollivier Ricci filtration to compute edge curvature values and persistence diagrams. "
-                )
-                self.curvature = ollivier_ricci_curvature
-                self.calculate_homology(filter_fn=ollivier_ricci_curvature)
+                if len(self.graph.nodes()) > 0:
+                    # Automatically Compute OR Curvature and corresponding Diagrams when changing a graph
+                    print(
+                        "Using Ollivier Ricci filtration to compute edge curvature values and persistence diagrams. "
+                    )
+                    self.curvature = ollivier_ricci_curvature
+                    self.calculate_homology(filter_fn=ollivier_ricci_curvature)
 
         else:
             print(
@@ -114,23 +117,26 @@ class MapperTopology:
                 "You must first define a graph representation for \
                 `X` via using `kmapper` before you can compute persistent diagrams"
             )
-        # Default to OR Curvature
-        if self._curvature is None:
-            "Computing edge curvature values"
-            self._curvature = filter_fn(self._graph)  # Set curvatures
+        elif len(self.graph.nodes()) > 0:
+            # Default to OR Curvature
+            if self._curvature is None:
+                "Computing edge curvature values"
+                self._curvature = filter_fn(self._graph)  # Set curvatures
 
-        G = make_node_filtration(
-            self._graph,
-            self._curvature,
-            attribute_name="curvature",
-            use_min=use_min,
-        )
-        pd = calculate_persistence_diagrams(
-            G,
-            "curvature",
-            "curvature",
-        )
-        self._diagram = pd
+            G = make_node_filtration(
+                self._graph,
+                self._curvature,
+                attribute_name="curvature",
+                use_min=use_min,
+            )
+            pd = calculate_persistence_diagrams(
+                G,
+                "curvature",
+                "curvature",
+            )
+            self._diagram = pd
+        else:
+            print("ERROR: This mapper computation produced a graph with 0 nodes")
 
     # TODO: Implement Visualization Methods. See starter code in ./nammu/
     def plot_curvature(self):
@@ -147,15 +153,17 @@ def curvature_analysis(
     perc_overlap,
     K,
     min_intersection_vals,
+    random_state=None,
 ):
 
     # Configure CoalMapper
     mapper = MapperTopology(X=X)
-    clusterer = KMeans(n_clusters=K, n_init="auto")
+    clusterer = KMeans(n_clusters=K, n_init="auto", random_state=random_state)
     cover = (n_cubes, perc_overlap)
     # Generate Graphs
     results = {}
 
+    print("Computing Curvature Values and Persistence Diagrams")
     for val in min_intersection_vals:
         mapper.set_graph(cover=cover, clusterer=clusterer, min_intersection=val)
         mapper.calculate_homology(filter_fn=ollivier_ricci_curvature, use_min=True)
