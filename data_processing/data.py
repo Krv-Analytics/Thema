f51c7819bfc9db149736dfa920c952bba36f2e6b
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-02-27 17:30:58 +0100

    working mongo pull script

---
+++ data_processing/data.py
@@ -0,0 +1,61 @@
+"""Pull Latest Data from MongoDB"""
+
+import argparse
+import os
+import numpy as np
+from dotenv import load_dotenv
+
+from accessMongo import mongo_pull
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    client = os.getenv("mongo_client")
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-d",
+        "--database",
+        type=str,
+        default="cleaned",
+        help="Select database to pull from Mongo.",
+    )
+    parser.add_argument(
+        "-c",
+        "--col",
+        type=str,
+        default="coal_mapper",
+        help="Select collection to pull from within `--database`.",
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        default="csv",
+        help="Select file type for local data.",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default="./local_data/",
+        help="Output Dir for results.",
+    )
+
+    args = parser.parse_args()
+    mongo_pull(
+        client,
+        database=args.database,
+        type=args.type,
+        col=args.col,
+        filepath=args.output,
+    )
+
+    assert os.path.isfile(
+        f"{args.output+args.col}.{args.type}"
+    ), "Failed to write data locally"
+    print(
+        f"Data successfully pulled from the `{args.col}` collection in the `{args.database}` Mongo database."
+    )
