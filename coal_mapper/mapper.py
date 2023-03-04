4d7f080649a951d812e460581dba2769a3d1b261
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-04 17:59:56 +0100

    consolidated objects into single file

--- coal_mapper/mapper.py
+++ coal_mapper/mapper.py
@@ -1,3 +1,5 @@
+"""Object file for Coal Mapper computations and analysis"""
+
 import numpy as np
 import networkx as nx
 import kmapper as km
@@ -5,6 +7,13 @@ import pandas as pd
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 import datetime
+import seaborn as sns
+
+
+from persim import plot_diagrams
+from nammu.topology import calculate_persistence_diagrams
+from nammu.curvature import ollivier_ricci_curvature, forman_curvature
+from nammu.utils import make_node_filtration
 
 from kmapper import KeplerMapper
 
@@ -36,8 +45,10 @@ class CoalMapper(KeplerMapper):
         self,
         n_cubes: int = 6,
         perc_overlap: float = 0.4,
-        projection=TSNE(random_state=None),
-        clusterer=KMeans(8, random_state=None),
+        projection=TSNE(
+            random_state=None,
+        ),
+        clusterer=KMeans(8, random_state=None, n_init="auto"),
     ):
         """
         A wrapper function for kmapper that generates a simplicial complex based on a given lens, cover, and clustering algorithm.
@@ -231,3 +242,145 @@ class CoalMapper(KeplerMapper):
         )
         print(f"Go to {path_html} for a visualization of your CoalMapper!")
         return path_html
+
+
+##############################################################################################################################################
+##############################################################################################################################################
+
+
+class MapperTopology:
+    """
+    Analyzing different mapper graphs using discrete curvature and persistenet homology.
+
+    """
+
+    def __init__(self, X: np.ndarray):
+
+        self.data = X
+        self._mapper = None
+        self._curvature = None
+        self._graph = None
+        self._diagram = None
+
+    @property
+    def mapper(self):
+        return self._mapper
+
+    @property
+    def graph(self):
+        return self._graph
+
+    @property
+    def curvature(self):
+        if self._curvature is None:
+            print(
+                "Curvature has not been computed yet. \
+                First generate a networkx Graph from the dataset via Mapper."
+            )
+        return self._curvature
+
+    @property
+    def diagram(self):
+        if self._diagram is None:
+            print(
+                "Persistent Homology has not been computed yet. \
+                First generate a networkx Graph from the dataset via Mapper."
+            )
+        return self._diagram
+
+    @curvature.setter
+    def curvature(self, curvature_fn):
+
+        if self._graph is None:
+            print(
+                "You must first define a graph representation for \
+                `X` via using `kmapper` before you can compute edge curvatures"
+            )
+        else:
+            try:
+                curvature = curvature_fn(self.graph)
+                assert len(curvature) == len(self._graph.edges())
+                self._curvature = curvature
+            except:
+                print("Invalid Curvature function")
+
+    def set_graph(
+        self,
+        cover,
+        clusterer=KMeans(5, n_init="auto", random_state=2023),
+        min_intersection: int = 1,
+    ):
+        """Generate a new networkX graph from Data via mapper. Recompute"""
+        # Check that a reasonable Cover is provided
+        if (len(cover) == 2) and type(cover) is tuple:
+            n_cubes, perc_overlap = cover
+            if type(min_intersection) is not int or min_intersection < 1:
+                print(
+                    "Invalid Minimum Intersection Parameter for Mapper. \
+                Defualt value (min_intersection=1) has been applied."
+                )
+                min_intersection = 1
+            else:
+                print("Computing Mapper Algorithm...")
+                # Save for looping over min_intersection
+                if self._mapper is not None:
+                    self._graph = self._mapper.to_networkx(min_intersection)
+                else:
+                    self._mapper = CoalMapper(X=self.data)
+                    self._mapper.clusterer = clusterer
+                    self._mapper.compute_mapper(n_cubes, perc_overlap)
+                print("Generating networkx Graph...")
+                self._graph = self._mapper.to_networkx(min_intersection)
+
+                if len(self.graph.nodes()) > 0:
+                    # Automatically Compute OR Curvature and corresponding Diagrams when changing a graph
+                    print(
+                        "Using Ollivier Ricci filtration to compute edge curvature values and persistence diagrams. "
+                    )
+                    self.curvature = ollivier_ricci_curvature
+                    self.calculate_homology(filter_fn=ollivier_ricci_curvature)
+
+        else:
+            print(
+                "Please enter a valid Cover of the form \
+                (n_cubes,perc_overlap)"
+            )
+
+    def calculate_homology(self, filter_fn, use_min=True):
+        if self._graph is None:
+            print(
+                "You must first define a graph representation for \
+                `X` via using `kmapper` before you can compute persistent diagrams"
+            )
+        elif len(self.graph.nodes()) > 0:
+            # Default to OR Curvature
+            if self._curvature is None:
+                "Computing edge curvature values"
+                self._curvature = filter_fn(self._graph)  # Set curvatures
+
+            G = make_node_filtration(
+                self._graph,
+                self._curvature,
+                attribute_name="curvature",
+                use_min=use_min,
+            )
+            pd = calculate_persistence_diagrams(
+                G,
+                "curvature",
+                "curvature",
+            )
+            self._diagram = pd
+        else:
+            print("ERROR: This mapper computation produced a graph with 0 nodes")
+
+    # TODO: Implement Visualization Methods. See starter code in ./nammu/
+    def plot_curvature(self):
+        # Look at filtration visualization script
+        return sns.histplot(self.curvature)
+
+    def plot_diagrams(self):
+        persim_diagrams = [
+            np.asarray(self.diagram[0]._pairs),
+            np.asarray(self.diagram[1]._pairs),
+        ]
+        return plot_diagrams(persim_diagrams, show=True)
