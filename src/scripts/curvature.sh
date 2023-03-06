9aba14ff02b347c4c380ec6d505c7dde8f895dc8
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-06 17:58:01 +0100

    driver file name change

--- src/scripts/curvature.sh
+++ src/scripts/curvature.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 # CURVATURE ANALYSIS
-    #TODO: automate paths. For now run from same directory as the python file ./scripts/curvature.sh to execute
 
-#TODO (Sid): Set these ranges as you like
 K_VALS=(2 3 4 5 6 7 8 9 10)
 N_CUBES=(2 3 4 5 6 7 8 9 10)
 PERC_OVERLAP=(0.2 0.3 0.4 0.5)
 MIN_INTERSECTION=(1 2 3) 
 
-#TODO(Sid): Configure Data Load
-    #Added stub here for reference
-DATA=../data_processing/local_data/coal_mapper
+DATA=../../data/coal_mapper_one_hot_scaled_TSNE.pkl
 
 for K_VAL in "${K_VALS[@]}"; do
     for N in "${N_CUBES[@]}"; do
         for P in "${PERC_OVERLAP[@]}"; do
 echo "Running curvature analysis on Mapper graphs generated from K = $K_VAL clusters and a cover with $N cubes that overlap by $P %."
-    poetry run python curvature_analysis.py --data ${DATA}                                     \
+    poetry run python compute_curvature.py --data ${DATA}                                     \
                                     -n ${N}                                                    \
                                     -p ${P}                                                    \
                                     -K ${K_VAL}                                                \
