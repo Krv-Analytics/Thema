c3d8ca17b3e751553cdf1b45dff9daf2b1ea4228
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-07 22:47:34 +0100

    added verbose arg

--- src/scripts/curvature.sh
+++ src/scripts/curvature.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 # CURVATURE ANALYSIS
 
-K_VALS=(2 3 4 5 6 7 8 9 10)
-N_CUBES=(2 3 4 5 6 7 8 9 10)
-PERC_OVERLAP=(0.2 0.3 0.4 0.5)
-MIN_INTERSECTION=(1 2 3) 
+#Run from src/ directory
 
-DATA=../../data/coal_mapper_one_hot_scaled_TSNE.pkl
+K_VALS=(3)
+N_CUBES=(8)
+PERC_OVERLAP=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
+MIN_INTERSECTION=(1) 
+
 
 for K_VAL in "${K_VALS[@]}"; do
     for N in "${N_CUBES[@]}"; do
         for P in "${PERC_OVERLAP[@]}"; do
 echo "Running curvature analysis on Mapper graphs generated from K = $K_VAL clusters and a cover with $N cubes that overlap by $P %."
-    poetry run python compute_curvature.py --data ${DATA}                                     \
+
+poetry run python compute_curvature.py                                                         \
                                     -n ${N}                                                    \
                                     -p ${P}                                                    \
                                     -K ${K_VAL}                                                \
-                                    --min_intersection ${MIN_INTERSECTION[@]}                        \
+                                    -v                                                         \
+                                    --min_intersection ${MIN_INTERSECTION[@]}                  \
                                    #Using Default min_intersection for now
         done
     done
