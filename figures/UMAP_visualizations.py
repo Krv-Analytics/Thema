d733d9f852003ef68391dda6f2121f11c2cd7e23
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-16 15:46:08 +0100

    cmd line argumnets, output messages, auto browser plot

--- figures/UMAP_visualizations.py
+++ figures/UMAP_visualizations.py
@@ -2,53 +2,84 @@ import argparse
 import os
 import sys
 import pandas as pd
+import pickle
 
 from umap import UMAP
 from plotly.subplots import make_subplots
-import plotly.graph_objects as go  
-import pickle
+import plotly.graph_objects as go
+import plotly.io as pio
+
+pio.renderers.default = "browser"
+
 
 def UMAP_grid(df, dists, neighbors):
-        '''function reads in a df, outputs a grid visualization with n by n UMAP projected dataset visualizations'''
-        # example function inputs
-        # dists = [0, 0.01, 0.05, 0.1, 0.5, 1]
-        # neighbors = [3, 5, 10, 20, 40]
-        
-        #TODO
-        # make the colors nicer/give more meaning here
-        # figure out a way around this .dropna() call that removes all rows with missing data
-        data=df.dropna()
+    """function reads in a df, outputs a grid visualization with n by n UMAP projected dataset visualizations"""
+    # example function inputs
+    # dists = [0, 0.01, 0.05, 0.1, 0.5, 1]
+    # neighbors = [3, 5, 10, 20, 40]
 
-        #generate subplot titles
-        fig = make_subplots(
-            rows=len(dists), 
-            cols=len(neighbors), 
-            column_titles = list(map(str, neighbors)),
-            x_title='n_neighbors',
-            row_titles = list(map(str, dists)),
-            y_title='min_dist',
-            vertical_spacing=0.05, horizontal_spacing=0.03,
-            shared_xaxes=True, shared_yaxes=True)
+    # TODO
+    # make the colors nicer/give more meaning here
+    # figure out a way around this .dropna() call that removes all rows with missing data
+    data = df.dropna()
+    assert type(dists) == list, "Not list"
+    assert type(neighbors) == list, "Not list"
+    print(f"Visualizing UMAP Grid Search! ")
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+    print(f"Choices for n_neighbors: {neighbors}")
+    print(f"Choices for m_dist: {dists}")
+    print(
+        "-------------------------------------------------------------------------------- \n"
+    )
 
-        #generate figure
-        for d in range (0, len(dists)):
-            for n in range(0, len(neighbors)):
-                umap_2d = UMAP(min_dist = dists[d], n_neighbors=neighbors[n], n_components=2, init='random', random_state=0)
-                proj_2d = umap_2d.fit_transform(data)
-                outdf = pd.DataFrame(proj_2d, columns = ['0', '1'])
-                fig.add_trace(
-                    go.Scatter(x=outdf['0'], y=outdf['1'], mode='markers',
-                            marker = dict(size=3,color='red')),
-                    row=d+1, col=n+1)
-            
+    # generate subplot titles
+    fig = make_subplots(
+        rows=len(dists),
+        cols=len(neighbors),
+        column_titles=list(map(str, neighbors)),
+        x_title="n_neighbors",
+        row_titles=list(map(str, dists)),
+        y_title="min_dist",
+        vertical_spacing=0.05,
+        horizontal_spacing=0.03,
+        shared_xaxes=True,
+        shared_yaxes=True,
+    )
 
-        fig.update_layout(template='simple_white', showlegend=False,
-                        font=dict(color="black"))
-        fig.update_xaxes(range=[-25, 25], showticklabels=False)
-        fig.update_yaxes(range=[-25, 25], showticklabels=False)
-        file_name = f"figures/UMAPgrid_min_dist({dists[0]}-{dists[len(dists)-1]})_neigh({neighbors[0]}-{neighbors[len(neighbors)-1]}).html"
-        fig.write_html(file_name)
-        return file_name
+    # generate figure
+    for d in range(0, len(dists)):
+        for n in range(0, len(neighbors)):
+            umap_2d = UMAP(
+                min_dist=dists[d],
+                n_neighbors=neighbors[n],
+                n_components=2,
+                init="random",
+                random_state=0,
+            )
+            proj_2d = umap_2d.fit_transform(data)
+            outdf = pd.DataFrame(proj_2d, columns=["0", "1"])
+            fig.add_trace(
+                go.Scatter(
+                    x=outdf["0"],
+                    y=outdf["1"],
+                    mode="markers",
+                    marker=dict(size=3, color="red"),
+                ),
+                row=d + 1,
+                col=n + 1,
+            )
+
+    fig.update_layout(
+        template="simple_white", showlegend=False, font=dict(color="black")
+    )
+    fig.update_xaxes(range=[-25, 25], showticklabels=False)
+    fig.update_yaxes(range=[-25, 25], showticklabels=False)
+    file_name = f"figures/UMAPgrid_min_dist({dists[0]}-{dists[len(dists)-1]})_neigh({neighbors[0]}-{neighbors[len(neighbors)-1]}).html"
+    fig.write_html(file_name)
+    pio.show(fig)
+    return file_name
 
 
 ######################################################################################################
@@ -72,33 +103,38 @@ if __name__ == "__main__":
     parser.add_argument(
         "-n",
         "--neighbors_list",
-        type=str,
+        type=int,
+        nargs="+",
         default=[3, 5, 10, 20, 40],
         help="Insert a list of n_neighbors to grid search",
     )
 
     parser.add_argument(
         "-d",
-        "--dists_list",
-        type=str,
+        "--min_dists",
+        type=float,
+        nargs="+",
         default=[0, 0.01, 0.05, 0.1, 0.5, 1],
         help="Insert a list of min_dists to grid search",
     )
- 
+
     args = parser.parse_args()
     this = sys.modules[__name__]
 
     assert os.path.isfile(args.path), "Invalid Input Data"
     # Load Dataframe
     with open(args.path, "rb") as f:
-        print("Reading pickle file")
         df = pickle.load(f)
 
-    name = UMAP_grid(df, dists=args.dists_list, neighbors=args.neighbors_list)
+    name = UMAP_grid(df, dists=args.min_dists, neighbors=args.neighbors_list)
 
-    print("\n-------------------------------------------------------------------------------- \n\n")
     print(
-        f"Successfully created the UMAP Grid Search Visualization, outputted to {name}')"
+        "\n################################################################################## \n\n"
+    )
+    print(
+        f"Successfully created the UMAP Grid Search Visualization\n Saved in {name} for future reference!"
     )
 
-    print("\n\n --------------------------------------------------------------------------------\n")
+    print(
+        "\n\n##################################################################################\n"
+    )
