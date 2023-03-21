e6d89da3a341811b1655a2273ff0011ee788c4e0
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-03-21 18:58:40 +0100

    argument edits to match refactoring

--- src/scripts/curvature.sh
+++ src/scripts/curvature.sh
@@ -3,25 +3,25 @@
 
 #Run from src/ directory
 
-# K_VALS=(6 7 8 9 10 11 12 13 14 15 )
-# N_CUBES=(7 8 9 10 11 12 13 14 15 16 17 18 19)
-# PERC_OVERLAP=(0.5)
-# MIN_INTERSECTION=(1 2 3 4 5 6 7 8 9 10) 
 
-K_VALS=(6)
+MIN_CLUSTER_SIZES=(10)
 N_CUBES=(7 8 9 10 11)
 PERC_OVERLAP=(0.5)
 MIN_INTERSECTION=(1) 
 
-for K_VAL in "${K_VALS[@]}"; do
+poetry shell
+echo "Initializing Poetry Shell"
+#TODO: Need to add umap to Poetry, for now need to run normal python within VE
+
+for MIN_CLUSTER_SIZE in "${MIN_CLUSTER_SIZES[@]}"; do
     for N in "${N_CUBES[@]}"; do
         for P in "${PERC_OVERLAP[@]}"; do
 echo "Running curvature analysis on Mapper graphs generated from K = $K_VAL clusters and a cover with $N cubes that overlap by $P %."
 
-poetry run python compute_curvature.py                                                         \
+python compute_curvature.py                                                         \
                                     -n ${N}                                                    \
                                     -p ${P}                                                    \
-                                    -K ${K_VAL}                                                \
+                                    --min_cluster_size ${MIN_CLUSTER_SIZE}                                                 \
                                     -v                                                         \
                                     --min_intersection ${MIN_INTERSECTION[@]}                  \
                                    #Using Default min_intersection for now
