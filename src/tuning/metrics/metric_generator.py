7282ab610eed0aa615fa6b7d87c7484185dcfff5
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-01 02:49:17 +0200

    tuning add, work in progress

---
+++ src/tuning/metrics/metric_generator.py
@@ -0,0 +1,69 @@
+import argparse
+import sys
+import os
+import pickle
+
+from metric_helper import topology_metric
+
+cwd = os.path.dirname(__file__)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-m",
+        "--metric",
+        type=str,
+        default="bottleneck",
+        help="Select metric (that is supported by Giotto) to compare persistence daigrams.",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--save_model",
+        default=True,
+        help="If True, save the clustering model and distances as pickle files.",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--Verbose",
+        default=False,
+        action="store_true",
+        help="If set, will print messages detailing computation and output.",
+    )
+
+    args = parser.parse_args()
+    this = sys.modules[__name__]
+
+    keys, distances = topology_metric(args.metric)
+
+    if args.save:
+        distance_file = f"curvature_{args.metric}_pairwise_distances.pkl"
+
+        out_dir_message = f"{distance_file} successfully written."
+
+        output_dir = os.path.join(cwd, "../data/parameter_models/")
+
+        # Check if output directory already exists
+        if os.path.isdir(output_dir):
+            distance_file = os.path.join(output_dir, distance_file)
+
+        else:
+            os.makedirs(output_dir, exist_ok=True)
+            distance_file = os.path.join(output_dir, distance_file)
+
+        with open(distance_file, "wb") as handle:
+            pickle.dump(distances, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        if args.Verbose:
+            print("\n")
+            print(
+                "-------------------------------------------------------------------------------- \n\n"
+            )
+            print(f"{out_dir_message}")
+
+            print(
+                "\n\n -------------------------------------------------------------------------------- "
+            )
