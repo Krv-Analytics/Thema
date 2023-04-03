c21696b7c2f817ae1552ab521e76d02ee5024ad2
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-03 02:37:51 +0200

    adjusting script to match file struct

--- scripts/mapper_grid_search.sh
+++ scripts/mapper_grid_search.sh
@@ -1,32 +1,29 @@
 #!/usr/bin/env bash
-# CURVATURE ANALYSIS
+# MAPPER GRID SEARCH
 
-#Run from src/ directory
+#Run from root/scripts/
 
 
 MIN_CLUSTER_SIZES=(6)
 N_CUBES=(3 4 5 6 7 8 9 10 11)
-PERC_OVERLAP=(0.3 0.35 0.4 0.45 0.5 0.55)
+PERC_OVERLAP=(0.45 0.5 0.55)
 MIN_INTERSECTION=(1)
 
 poetry shell
 echo "Initializing Poetry Shell"
 #TODO: Need to add umap to Poetry, for now need to run normal python within VE
-
 for MIN_CLUSTER_SIZE in "${MIN_CLUSTER_SIZES[@]}"; do
     for N in "${N_CUBES[@]}"; do
         for P in "${PERC_OVERLAP[@]}"; do
-echo "Running curvature analysis on Mapper graphs generated from K = $K_VAL clusters and a cover with $N cubes that overlap by $P %."
-
-python compute_curvature.py                                                         \
+            echo "Running curvature analysis on Mapper graphs generated from K = $K_VAL clusters and a cover with $N cubes that overlap by $P %."
+            python ../src/modeling/coal_mapper_generator.py                                    \
                                     -n ${N}                                                    \
                                     -p ${P}                                                    \
-                                    --min_cluster_size ${MIN_CLUSTER_SIZE}                                                 \
+                                    --min_cluster_size ${MIN_CLUSTER_SIZE}                     \
                                     -v                                                         \
                                     --min_intersection ${MIN_INTERSECTION[@]}                  \
                                    #Using Default min_intersection for now
         done
     done
 done
-
 exit 0
