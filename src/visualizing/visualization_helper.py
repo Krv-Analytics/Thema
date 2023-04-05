04e15f8a312717c39b8773cf20794611bc3c2b5d
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-06 00:55:27 +0200

    dumping tuckaho funland in here

--- src/visualizing/visualization_helper.py
+++ src/visualizing/visualization_helper.py
@@ -1,80 +1,59 @@
 "Helper functions for visualizing CoalMappers"
 
-import datetime
-import os
-import sys
+import time
 import numpy as np
 import pandas as pd
 
+import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.io as pio
-from dotenv import load_dotenv
 from plotly.subplots import make_subplots
-from sklearn.preprocessing import MinMaxScaler
-from persim import plot_diagrams
-import matplotlib.pyplot as plot
-import seaborn as sns
-
-load_dotenv()
-root = os.getenv("root")
-src = os.getenv("src")
-sys.path.append(src)
-
-from processing.cleaning.tupper import Tupper
-from modeling.coal_mapper import CoalMapper
+import plotly.express as px
+from scipy.cluster.hierarchy import dendrogram
 
 
-def plot(
-    coal_mapper: CoalMapper,
-):
-    """"""
-    assert (
-        len(coal_mapper.complex) > 0
-    ), "First run `fit()` to generate a nonempty simplicial complex."
-
-    path_html = mapper_plot_outfile(coal_mapper.n_cubes, coal_mapper.perc_overlap)
-
-    numeric_data, labels = config_plot_data(coal_mapper.tupper)
-    colorscale = custom_color_scale()
-
-    coal_mapper.mapper.visualize(
-        coal_mapper.complex,
-        node_color_function=["mean", "median", "std", "min", "max"],
-        color_values=numeric_data,
-        color_function_name=labels,
-        colorscale=colorscale,
-        path_html=path_html,
-    )
-    print(f"Go to {path_html} for a visualization of your CoalMapper!")
-    return path_html
+from umap import UMAP
+import hdbscan
 
 
-def plot_curvature(coal_mapper: CoalMapper, bins="auto", kde=False):
-    """Visualize Curvature of a mapper graph as a histogram."""
-
-    ax = sns.histplot(
-        coal_mapper.curvature,
-        discrete=True,
-        stat="probability",
-        kde=kde,
-        bins=bins,
-    )
-    ax.set(xlabel="Ollivier Ricci Edge Curvatures")
-
-    return ax
+from sklearn.cluster import KMeans, MiniBatchKMeans
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.decomposition import PCA
 
 
-def plot_diagrams(coal_mapper):
-    """Visualize persistence diagrams of a mapper graph."""
-    persim_diagrams = [
-        np.asarray(coal_mapper.diagram[0]._pairs),
-        np.asarray(coal_mapper.diagram[1]._pairs),
-    ]
-    return plot_diagrams(persim_diagrams, show=True)
+pio.renderers.default = "browser"
 
 
-def connected_component_heatmaps(coal_mapper):
-    viz = coal_mapper.data
+def plot_dendrogram(model, labels, distance, **kwargs):
+    """Create linkage matrix and then plot the dendrogram for Hierarchical clustering."""
+
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    d = dendrogram(linkage_matrix, labels=labels, **kwargs)
+    plt.title("Hyperparameter Dendrogram")
+    plt.xlabel("Coordinates: (n_cubes,perc_overlap,min_intersection).")
+    plt.ylabel(f"{distance} distance between persistence diagrams")
+    plt.show()
+    return d
+
+
+def connected_component_heatmaps(self):
+    viz = self.mapper.data
 
     targetCols = [
         "NAMEPCAP",
@@ -160,59 +139,33 @@ def connected_component_heatmaps(coal_mapper):
     pio.show(fig)
 
 
-def plot_dendrogram(model, labels, distance, **kwargs):
-    """Create linkage matrix and then plot the dendrogram for Hierarchical clustering."""
+def UMAP_grid(df, dists, neighbors):
+    """function reads in a df, outputs a grid visualization with n by n UMAP projected dataset visualizations
 
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
+    grid search the UMAP parameter space, choose the representations that occur most often in the given parameter space, based on the generated histogram"""
+    # example function inputs
+    # dists = [0, 0.01, 0.05, 0.1, 0.5, 1]
+    # neighbors = [3, 5, 10, 20, 4_0]
 
-    linkage_matrix = np.column_stack(
-        [model.children_, model.distances_, counts]
-    ).astype(float)
+    # TODO
+    # marl outlying points in a different color - check the cluster_distribution output
+    # figure out a way around this .dropna() call that removes all rows with missing data
+    data = df.dropna()
+    assert type(dists) == list, "Not list"
+    assert type(neighbors) == list, "Not list"
+    print(f"Visualizing UMAP Grid Search! ")
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+    print(f"Choices for n_neighbors: {neighbors}")
+    print(f"Choices for m_dist: {dists}")
+    print(
+        "-------------------------------------------------------------------------------- \n"
+    )
 
-    # Plot the corresponding dendrogram
-    d = dendrogram(linkage_matrix, labels=labels, **kwargs)
-    plt.title("Hyperparameter Dendrogram")
-    plt.xlabel("Coordinates: (n_cubes,perc_overlap,min_intersection).")
-    plt.ylabel(f"{distance} distance between persistence diagrams")
-    plt.show()
-    return d
-
-
-def mapper_plot_outfile(n, p):
-    time = int(datetime.datetime.now().timestamp())
-
-    output_file = f"mapper_ncubes{n}_{int(p*100)}perc_{time}.html"
-    output_dir = os.path.join(root, "data/visualizations/mapper_htmls/")
-
-    if os.path.isdir(output_dir):
-        output_file = os.path.join(output_dir, output_file)
-    else:
-        os.makedirs(output_dir, exist_ok=True)
-        output_file = os.path.join(output_dir, output_file)
-
-    return output_file
-
-
-def config_plot_data(tupper: Tupper):
-    temp_data = tupper.clean
-    string_cols = temp_data.select_dtypes(exclude="number").columns
-    numeric_data = temp_data.drop(string_cols, axis=1).dropna()
-    labels = list(numeric_data.columns)
-    return numeric_data, labels
-
-
-def custom_color_scale():
-    colorscale = [
-        [0.0, "#001219"],
+    # define custom color scale
+    map_colors = [
+        [0.0, "#043e4a"],
         [0.1, "#005f73"],
         [0.2, "#0a9396"],
         [0.3, "#94d2bd"],
@@ -220,8 +173,346 @@ def custom_color_scale():
         [0.5, "#ee9b00"],
         [0.6, "#ca6702"],
         [0.7, "#bb3e03"],
-        [0.8, "#ae2012"],
-        [0.9, "#9b2226"],
-        [1.0, "#a50026"],
+        [0.8, "#e82f1e"],
+        [0.9, "#a30309"],
+        [1.0, "#780a23"],
     ]
-    return colorscale
+
+    # generate subplot titles
+    fig = make_subplots(
+        rows=len(dists),
+        cols=len(neighbors),
+        column_titles=list(map(str, neighbors)),
+        x_title="n_neighbors",
+        row_titles=list(map(str, dists)),
+        y_title="min_dist",
+        vertical_spacing=0.05,
+        horizontal_spacing=0.03,
+        # shared_xaxes=True,
+        # shared_yaxes=True,
+    )
+    cluster_distribution = []
+    # generate figure
+    for d in range(0, len(dists)):
+        for n in range(0, len(neighbors)):
+            umap_2d = UMAP(
+                min_dist=dists[d],
+                n_neighbors=neighbors[n],
+                n_components=2,
+                init="random",
+                random_state=0,
+            )
+            proj_2d = umap_2d.fit_transform(data)
+            clusterer = hdbscan.HDBSCAN(min_cluster_size=5).fit(proj_2d)
+            outdf = pd.DataFrame(proj_2d, columns=["0", "1"])
+            outdf["labels"] = clusterer.labels_
+
+            num_clusters = len(np.unique(clusterer.labels_))
+            cluster_distribution.append(num_clusters)
+            df = outdf[outdf["labels"] != -1]
+            fig.add_trace(
+                go.Scatter(
+                    x=df["0"],
+                    y=df["1"],
+                    mode="markers",
+                    marker=dict(
+                        size=4,
+                        color=df["labels"],
+                        cmid=0.5,
+                        colorscale="Turbo",  # colorscale=map_colors
+                    ),
+                    hovertemplate=df["labels"],
+                ),
+                row=d + 1,
+                col=n + 1,
+            )
+
+            df = outdf[outdf["labels"] == -1]
+            fig.add_trace(
+                go.Scatter(
+                    x=df["0"],
+                    y=df["1"],
+                    mode="markers",
+                    marker=dict(
+                        size=4,
+                        color="yellow",
+                        line=dict(width=0.1, color="DarkSlateGrey"),
+                    ),
+                    hovertemplate=df["labels"],
+                ),
+                row=d + 1,
+                col=n + 1,
+            )
+
+    fig.update_layout(
+        template="simple_white", showlegend=False, font=dict(color="black")
+    )
+
+    fig.update_xaxes(showticklabels=False, tickwidth=0, tickcolor="rgba(0,0,0,0)")
+    fig.update_yaxes(showticklabels=False, tickwidth=0, tickcolor="rgba(0,0,0,0)")
+
+    file_name = f"figures/UMAPgrid_min_dist({dists[0]}-{dists[len(dists)-1]})_neigh({neighbors[0]}-{neighbors[len(neighbors)-1]}).html"
+    fig.write_html(file_name)
+    pio.show(fig)
+
+    # ax = sns.histplot(
+    #     cluster_distribution,
+    #     discrete=True,
+    #     stat="percent",
+    # )
+    # ax.set(xlabel="Num_Clusters based on HDBSCAN", Num_Clusters based on HDBSCAN)
+    # plt.show()
+
+    colors = pd.DataFrame(cluster_distribution, columns=["Num"])
+    fig2 = px.histogram(
+        colors,
+        x="Num",
+        color="Num",
+        nbins=max(cluster_distribution),
+        color_discrete_map={
+            1: "#005f73",
+            2: "#0a9396",
+            3: "#94d2bd",
+            4: "#e9d8a6",
+            5: "#ee9b00",
+            6: "#ca6702",
+            7: "#bb3e03",
+            8: "#e82f1e",
+        },
+    )
+    fig2.update_layout(
+        template="simple_white",
+        showlegend=False,
+        xaxis_title="Num_Clusters based on HDBSCAN",
+        title="Cluster Histogram",
+    )
+    pio.show(fig2)
+
+    return file_name
+
+
+#### IMPORTANT FUNCTIONS
+def ORISPLmerge(df1, df2):
+    return df1.merge(df2, how="left", left_on="ORISPL", right_on="ORISPL")
+
+
+######
+#### OTHER FUNCTIONS
+######
+
+
+def coal_PCA(df: str):
+    """
+    Graphs a Principle component analysis with 2 principle components
+    df can only consist of numbers, no words/labels. Ensure you supply a subset of the dataset that conforms to this restriction"""
+    x = df
+    y = StandardScaler().fit_transform(x)
+
+    # apply PCA
+    pca = PCA(n_components=2)
+    X = pca.fit_transform(y)
+
+    loadings = pd.DataFrame(
+        pca.components_.T, columns=["PC1", "PC2"], index=list(x.columns)
+    )
+    loadings
+
+    def loading_plot(coeff, labels):
+        n = coeff.shape[0]
+        for i in range(n):
+            plt.arrow(
+                0,
+                0,
+                coeff[i, 0],
+                coeff[i, 1],
+                head_width=0.05,
+                head_length=0.05,
+                color="#21918C",
+                alpha=0.5,
+            )
+            plt.text(
+                coeff[i, 0] * 1.15,
+                coeff[i, 1] * 1.15,
+                labels[i],
+                color="#21918C",
+                ha="center",
+                va="center",
+            )
+        plt.xlim(-1, 1)
+        plt.ylim(-1, 1)
+        plt.xlabel("PC1")
+        plt.ylabel("PC2")
+        plt.grid()
+
+    fig, ax = plt.subplots(figsize=(7, 7))
+    loading_plot(pca.components_.T, list(x.columns))
+
+
+def mapper_labels(df: pd.DataFrame):
+
+    """This function takes in dataframe to use as the data labels for the mapper.
+    Returns a dataframe
+    """
+
+    names = df.to_numpy()
+    return names
+
+
+def view_kmeans(df: str, numclusters: int):
+
+    # data
+
+    X = StandardScaler().fit_transform(df)
+
+    # #############################################################################
+    # Clustering with KMeans
+
+    k_means = KMeans(init="k-means++", n_clusters=numclusters, n_init=10)
+    t0 = time.time()
+    k_means.fit(X)
+    t_batch = time.time() - t0
+
+    # #############################################################################
+    # Clustering with MiniBatchKMeans
+
+    mbk = MiniBatchKMeans(
+        init="k-means++",
+        n_clusters=numclusters,
+        batch_size=numclusters,
+        n_init=10,
+        max_no_improvement=10,
+        verbose=0,
+    )
+    t0 = time.time()
+    mbk.fit(X)
+    t_mini_batch = time.time() - t0
+
+    # #############################################################################
+    # Plot result
+
+    fig = plt.figure(figsize=(8, 3))
+    fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
+    colors = ["#4EACC5", "#FF9C34", "#4E9A06", "#3082be"]
+
+    # COLOR MiniBatchKMeans and the KMeans algorithm.
+    k_means_cluster_centers = k_means.cluster_centers_
+    order = pairwise_distances_argmin(k_means.cluster_centers_, mbk.cluster_centers_)
+    mbk_means_cluster_centers = mbk.cluster_centers_[order]
+
+    k_means_labels = pairwise_distances_argmin(X, k_means_cluster_centers)
+    mbk_means_labels = pairwise_distances_argmin(X, mbk_means_cluster_centers)
+
+    # KMeans
+    ax = fig.add_subplot(1, 3, 1)
+    for k, col in zip(range(numclusters), colors):
+        my_members = k_means_labels == k
+        cluster_center = k_means_cluster_centers[k]
+        ax.plot(
+            X[my_members, 0], X[my_members, 1], "w", markerfacecolor=col, marker="."
+        )
+        ax.plot(
+            cluster_center[0],
+            cluster_center[1],
+            "o",
+            markerfacecolor=col,
+            markeredgecolor="k",
+            markersize=6,
+        )
+    ax.set_title("KMeans")
+    ax.set_xticks(())
+    ax.set_yticks(())
+    plt.text(-3.5, 1.8, "train time: %.2fs\ninertia: %f" % (t_batch, k_means.inertia_))
+
+    # MiniBatchKMeans
+    ax = fig.add_subplot(1, 3, 2)
+    for k, col in zip(range(numclusters), colors):
+        my_members = mbk_means_labels == k
+        cluster_center = mbk_means_cluster_centers[k]
+        ax.plot(
+            X[my_members, 0], X[my_members, 1], "w", markerfacecolor=col, marker="."
+        )
+        ax.plot(
+            cluster_center[0],
+            cluster_center[1],
+            "o",
+            markerfacecolor=col,
+            markeredgecolor="k",
+            markersize=6,
+        )
+    ax.set_title("MiniBatchKMeans")
+    ax.set_xticks(())
+    ax.set_yticks(())
+    plt.text(-3.5, 1.8, "train time: %.2fs\ninertia: %f" % (t_mini_batch, mbk.inertia_))
+
+    # Initialise the different array to all False
+    different = mbk_means_labels == 4
+    ax = fig.add_subplot(1, 3, 3)
+
+    for k in range(numclusters):
+        different += (k_means_labels == k) != (mbk_means_labels == k)
+
+    identic = np.logical_not(different)
+    ax.plot(X[identic, 0], X[identic, 1], "w", markerfacecolor="#bbbbbb", marker=".")
+    ax.plot(X[different, 0], X[different, 1], "w", markerfacecolor="m", marker=".")
+    ax.set_title("Difference")
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+    plt.show()
+
+
+def percentCO2reduxMap(mapdf: pd.DataFrame, all: pd.DataFrame):
+    def make_chloropheth(subset: pd.DataFrame, all: pd.DataFrame):
+        state_total = (
+            all[all["decom"] == 0].groupby("PSTATABB")["PLCO2AN"].sum().reset_index()
+        )
+        state_reduction = (
+            subset[subset["decom"] == 0]
+            .groupby("PSTATABB")["PLCO2AN"]
+            .sum()
+            .reset_index()
+        )
+        state_total = state_total.merge(
+            state_reduction, how="left", on="PSTATABB"
+        ).fillna(0)
+        state_total["percentRedux"] = (
+            state_total["PLCO2AN_y"] / state_total["PLCO2AN_x"]
+        )
+        return state_total
+
+    state_total = make_chloropheth(mapdf, all)
+
+    num = len(mapdf)
+    redux = int(
+        100 * (mapdf["PLCO2AN"].sum() / all[all["decom"] == 0]["PLCO2AN"].sum())
+    )
+
+    fig = go.Figure(
+        data=[
+            go.Choropleth(
+                locations=state_total["PSTATABB"],  # Spatial coordinates
+                z=state_total["percentRedux"].astype(float),  # Data to be color-coded
+                locationmode="USA-states",  # set of locations match entries in `locations`
+                colorscale="earth",
+                colorbar_title="% State Reduction",
+            ),
+            go.Scattergeo(
+                lon=mapdf["LON"],
+                lat=mapdf["LAT"],
+                text=mapdf["label"],
+                marker=dict(
+                    color=list(range(0)),
+                    colorscale="viridis",
+                    size=mapdf["PLNGENAN"] / 400000,
+                ),
+                # marker_color = df['CO2limits'],
+            ),
+        ]
+    )
+
+    fig.update_layout(
+        title_text=f"{num} Plant Closures - {redux}% CO2 Reduction Nationwide",
+        geo_scope="usa",  # limite map scope to USA
+    )
+
+    fig.show()
