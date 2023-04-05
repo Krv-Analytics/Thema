c304c85eb225418e6d0c2422f68e89cfc2c39f0f
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-05 19:45:14 +0200

    cleaning imports

--- src/modeling/model.py
+++ src/modeling/model.py
@@ -1,49 +1,46 @@
-import numpy as np
-import pandas as pd
-
 class Model:
-    def __init__(self,tupper):
+    def __init__(self, tupper):
 
         self._tupper = tupper
 
-        #Mapper Node Attributes
+        # Mapper Node Attributes
         self._node_id = None
         self._node_description = None
 
         # Mapper Cluster
         self._cluster_ids = None
         self._cluster_description = None
-        
 
     @property
     def cluster_ids(self):
         return self._cluster_ids
+
     @cluster_ids.setter
-    def cluster_ids(self,ids):
+    def cluster_ids(self, ids):
         self._cluster_ids = ids
-        return self.cluster_ids 
-    
-    @property 
+        return self.cluster_ids
+
+    @property
     def cluster_description(self):
         return self._cluster_description
-    @cluster_description.setter 
-    def cluster_description(self,description):
+
+    @cluster_description.setter
+    def cluster_description(self, description):
         self._cluster_description = description
-    
 
     @property
     def node_ids(self):
         return self._node_ids
+
     @node_ids.setter
-    def node_ids(self,ids):
+    def node_ids(self, ids):
         self._node_ids = ids
-        return self.node_ids 
-    
-    @property 
+        return self.node_ids
+
+    @property
     def node_description(self):
         return self._node_description
-    @node_description.setter 
-    def node_description(self,description):
+
+    @node_description.setter
+    def node_description(self, description):
         self._node_description = description
-
-
