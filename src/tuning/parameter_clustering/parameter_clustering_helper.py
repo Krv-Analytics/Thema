2df6a5dbbc3ca1006c6832db13b636589a4b1e28
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-05 19:45:28 +0200

    move plotting to visualizations/

--- src/tuning/parameter_clustering/parameter_clustering_helper.py
+++ src/tuning/parameter_clustering/parameter_clustering_helper.py
@@ -25,33 +25,6 @@ def cluster_hyperparams(keys, distances, metric, p_frac=0.1):
     return model
 
 
-def plot_dendrogram(model, labels, distance, **kwargs):
-    """Create linkage matrix and then plot the dendrogram for Hierarchical clustering."""
-
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-
-    linkage_matrix = np.column_stack(
-        [model.children_, model.distances_, counts]
-    ).astype(float)
-
-    # Plot the corresponding dendrogram
-    d = dendrogram(linkage_matrix, labels=labels, **kwargs)
-    plt.title("Hyperparameter Dendrogram")
-    plt.xlabel("Coordinates: (n_cubes,perc_overlap,min_intersection).")
-    plt.ylabel(f"{distance} distance between persistence diagrams")
-    plt.show()
-    return d
-
-
 def read_distance_matrices(metric):
     dir = os.path.join(cwd, "./../../../data/parameter_modeling/distance_matrices/")
     file = os.path.join(dir, f"curvature_{metric}_pairwise_distances.pkl")
