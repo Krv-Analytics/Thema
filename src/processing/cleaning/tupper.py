4d750244fa2fe3efcedb32fc1037cf7ac79a59eb
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-04-05 05:12:46 +0200

    tupper class work in progress

--- src/processing/cleaning/tupper.py
+++ src/processing/cleaning/tupper.py
@@ -1,15 +1,45 @@
 "Dishwasher safe data container"
+from os.path import isfile
+import pickle
 
 
 class Tupper:
-    def __init__(self, dirty, clean):
-        self._dirty = dirty
-        self._clean = clean
+    def __init__(self, raw: str, clean: str, projection: str):
+
+        self._raw = None
+        self._clean = None
+        self._projection = None
+
+        # If files exist, set members
+        if isfile(raw):
+            self._raw = raw
+        if isfile(raw):
+            self._clean = clean
+        if isfile(projection):
+            self._projection = projection
 
     @property
-    def dirty(self):
-        return self._dirty
+    def raw(self):
+        assert self._raw, "Please Specify a valid path to raw data"
+        with open(self._raw, "rb") as raw_file:
+            raw_df = pickle.load(raw_file)
+
+        return raw_df
 
     @property
     def clean(self):
-        return self._clean
+        assert self._clean, "Please Specify a valid path to clean data"
+        with open(self._raw, "rb") as clean_file:
+            reference = pickle.load(clean_file)
+        clean_df = reference["clean_data"]
+        # dropped_columns = reference["dropped_columns"]
+        return clean_df
+
+    @property
+    def projection(self):
+        assert self._projection, "Please Specify a valid path to clean data"
+        with open(self._projection, "rb") as projection_file:
+            reference = pickle.load(projection_file)
+            projection_array = reference["projection"]
+
+        return projection_array
