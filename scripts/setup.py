025f0dcd61783ce206e6bb5ccd57708039356138
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-05-23 21:46:56 +0200

    moving mongo config to .env

--- scripts/setup.py
+++ scripts/setup.py
@@ -8,4 +8,10 @@ env_file = os.path.join(root, ".env")
 with open(env_file, "w") as f:
     f.write(f"root='{root}/'\n")
     f.write(f"src='{src}'\n")
-    f.write("mongo_client=''")
+    f.write("mongo_client='' #Your Mongo Client ID\n")
+    f.write(
+        "mongo_database='' #The name of the MongoDB database where the dataset is located.\n"
+    )
+    f.write(
+        "mongo_collection='' #The name of the collection within the specified MongoDB database."
+    )
