f8385c136b1b35a5d98b4342d96c1fbcbb978a67
Stuart Wayland <stuartwayland@ucsb.edu>
2023-05-24 17:31:07 -0700

    Stub code for test_jmapper.py

--- tests/modeling/test_jmapper.py
+++ tests/modeling/test_jmapper.py
@@ -12,6 +12,13 @@ import numpy as np
 import random
 import pickle
 
+import kmapper as km
+import networkx as nx
+import numpy as np
+from hdbscan import HDBSCAN
+from kmapper import KeplerMapper
+ 
+
 from dotenv import load_dotenv
 
 # ##############################################################################################################################
@@ -21,20 +28,81 @@ from dotenv import load_dotenv
 load_dotenv()
 path_to_src = os.getenv("src")
 sys.path.append(path_to_src)
+sys.path.append(path_to_src + "/modeling/")
 
 import modeling as md
+from nammu.curvature import ollivier_ricci_curvature
+from nammu.topology import PersistenceDiagram, calculate_persistence_diagrams
+from nammu.utils import make_node_filtration
 
 ################################################################################################################################
 #
-# Initilizing Data for Unit Tests 
+# TODO: Summarize neccessary unit tests 
 #
-#  Unit Tests    
-#       1) Empty initialization (invalid paths ) 
-#       2) Unsupported file types (ie non-pickle files) 
-#       3) Randomly generated raw/clean/projected (correctness)
+#  Outline of Necessary Unit Tests    
+#       1) Empty initialization (Edge cases ie empty mappers, invalid tuppers, etc) 
 #
 ################################################################################################################################
 
 #
 # Setting Temporary Testing Files 
 #
+
+# TODO 
+# Create the necessary files to initialize mappers to serve as unit tests 
+
+
+################################################################################################################################
+
+
+# TODO: Complete these Stubs! 
+
+class TestJMapper:
+    def test_init(self):
+        # Stub
+        assert 1==1
+    def test_tupper(self):
+        # Stub 
+        assert 1==1
+    def test_mapper(self):
+        # Stub 
+        assert 1==1
+    def test_cover(self):
+        # Stub 
+        assert 1==1
+    def test_clusterer(self):
+        # Stub 
+        assert 1==1
+    def test_complex(self):
+        # Stub 
+        assert 1==1
+    def test_graph(self):
+         # Stub 
+        assert 1==1
+    def test_min_intersection(self):
+         # Stub 
+        assert 1==1
+    def test_components(self):
+         # Stub 
+        assert 1==1
+    def test_num_policy_groups(self):
+         # Stub 
+        assert 1==1
+    def test_curvature(self):
+         # Stub 
+        assert 1==1
+    def test_diagram(self):
+         # Stub 
+        assert 1==1
+    def test_to_networkx(self):
+         # Stub 
+        assert 1==1
+    def test_connected_components(self):
+         # Stub 
+        assert 1==1
+    def test_calculate_homology(self):
+         # Stub 
+        assert 1==1
+    def test_item_lookup(self):
+        # Stub 
+        assert 1==1 
