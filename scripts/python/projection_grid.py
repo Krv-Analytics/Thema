cd33ea8d042a8e3e0af0b2973ee637140519ff7e
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-06-01 12:37:05 +0200

    adding python scripts for grid searches

---
+++ scripts/python/projection_grid.py
@@ -0,0 +1,40 @@
+import json
+import logging
+import os
+import subprocess
+
+from dotenv import load_dotenv
+from python_log_indenter import IndentedLoggerAdapter
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    log = IndentedLoggerAdapter(logging.getLogger(__name__))
+    load_dotenv()
+    root = os.getenv("root")
+    src = os.getenv("src")
+    JSON_PATH = os.getenv("params")
+    if os.path.isfile(JSON_PATH):
+        with open(JSON_PATH, "r") as f:
+            params_json = json.load(f)
+    else:
+        print("params.json file note found!")
+
+    N_neighbors = params_json["projector_Nneighbors"]
+    min_Dists = params_json["projetor_minDists"]
+
+    projector = os.path.join(src, "processing/projecting/projector.py")
+    log.info("Computing UMAP Projection Grid Search!")
+    log.info(
+        "--------------------------------------------------------------------------------"
+    )
+    log.info(f"Choices for n_neighbors: {N_neighbors}")
+    log.info(f"Choices for min_dist: {min_Dists}")
+    log.info(
+        "--------------------------------------------------------------------------------"
+    )
+    log.add()
+    ## GRID SEARCH PROJECTIONS
+    for n in N_neighbors:
+        for d in min_Dists:
+            subprocess.run(["python", f"{projector}", f"-n {n}", f"-d {d}"])
---
+++ scripts/python/setup.py
@@ -0,0 +1,21 @@
+# Needs to be run from the root directory!
+import os
+
+root = os.getcwd()
+src = os.path.join(root, "src/")
+params = os.path.join(root, "params.json")
+
+env_file = os.path.join(root, ".env")
+# Write .env file
+with open(env_file, "w") as f:
+    f.write(f"root='{root}/'\n")
+    f.write(f"src='{src}'\n")
+    f.write("mongo_client='' #Your Mongo Client ID\n")
+    f.write(
+        "mongo_database='' #The name of the MongoDB database where the dataset is located.\n"
+    )
+    f.write(
+        "mongo_collection='' #The name of the collection within the specified MongoDB database.\n"
+    )
+
+    f.write(f"params='{params}'")
