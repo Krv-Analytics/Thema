b28f6283d9a2f4544f70ce0c509417f10f3f42c1
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-06-12 18:33:12 +0200

    automatic model selection for all policy groups

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ PARAMS_JSON := $(shell cat $(PARAMS_FILE))
 RUN_NAME := $(shell echo '$(PARAMS_JSON)' | jq -r '.Run_Name')
 COVERAGE_FILTER := $(shell echo '$(PARAMS_JSON)' | jq -r '.coverage_filter')
 
+all: process-data projections models model-selection model-selection
+	@echo "Process complete"
 
 install: check-poetry 
 	@echo " Generating and populating .env file..."
@@ -34,25 +36,42 @@ process-data:
 projections: 
 	cd scripts/bash && ./projector.sh
 
+summarize-projections:
+	python src/summarizing/projection_summarizer.py
+
 models: 
 	cd scripts/bash && ./model_generator.sh 
 
 model-histogram:
 	cd scripts/python && python model_histogram.py
 
-curvature-histogram:
+curvature-distances:
+	cd scripts/python && python curvature_distance_generator.py
+
+curvature-histogram: curvature-distances
 	cd scripts/python && python curvature_histogram.py
+
+stability-histogram:
+	cd scripts/python && python stability_histogram.py
+
 dendrogram:
 	cd scripts/bash && ./dendrogram.sh
-model-selection:
-	cd scripts/bash && ./model_selector.sh
+
+model-clustering: curvature-distances
+	cd scripts/python && python clusterer.py
+
+model-selection: model-clustering
+	cd scripts/python && python selector.py
 
 
 
 # Cleaning commands for data fields 
 
+clean: clean-processed-data clean-projections clean-models clean-model-analysis clean-final-models
+	rm -f -r data/$(RUN_NAME)/
+
 clean-processed-data: 
-	rm -f data/$(RUN_NAME)/clean/*
+	rm -f -r data/$(RUN_NAME)/clean/*
 
 clean-projections:
 	rm -f -r data/${RUN_NAME}/projections/*
@@ -66,17 +85,10 @@ clean-model-analysis:
 clean-final-models:
 	rm -f -r  data/${RUN_NAME}/final_models/
 
-clean: clean-processed-data clean-projections 
+clean-raw-data: 
 	rm -f -r data/${RUN_NAME}/
 
-clean-raw-data: 
-	rm -f data/raw/* 
-	rm -f -r data/${RUN_NAME}/
-
-clean-raw-data: 
-	rm -f data/raw/* 
-
-clean-full : clean-raw-data clean-processed-data clean-projections 
+clean-all :
 	rm -f -r data/
 
 
---
+++ scripts/python/selector.py
@@ -0,0 +1,82 @@
+import json
+import logging
+import os
+import subprocess
+import sys
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dotenv import load_dotenv
+from python_log_indenter import IndentedLoggerAdapter
+from tqdm import tqdm
+
+load_dotenv()
+src = os.getenv("src")
+sys.path.append(src)
+sys.path.append(src + "modeling/")
+
+from modeling.model_selector_helper import unpack_policy_group_dir
+
+if __name__ == "__main__":
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+    log = IndentedLoggerAdapter(logging.getLogger(__name__))
+    load_dotenv()
+    root = os.getenv("root")
+    src = os.getenv("src")
+    JSON_PATH = os.getenv("params")
+    if os.path.isfile(JSON_PATH):
+        with open(JSON_PATH, "r") as f:
+            params_json = json.load(f)
+    else:
+        print("params.json file note found!")
+
+    dir = "data/" + params_json["Run_Name"] + f"/models/"
+    dir = os.path.join(root, dir)
+    group_ranks = []
+    for folder in os.listdir(dir):
+        i = unpack_policy_group_dir(folder)
+        group_ranks.append(i)
+
+    # Metric Generator Configuratiosn
+    model_selector = os.path.join(src, "modeling/model_selector.py")
+    model_clusterer = os.path.join(src, "tuning/graph_clustering/model_clusterer.py")
+    coverage = params_json["coverage_filter"]
+
+    # LOGGING
+    log.info("Selecting Models!")
+    log.info(
+        "--------------------------------------------------------------------------------"
+    )
+    log.info(f"Policy Groups in consideration: {group_ranks}")
+    log.info(f"Model Coverage Filter: {coverage}")
+    log.info(
+        "--------------------------------------------------------------------------------"
+    )
+    log.add()
+
+    # Number of loops
+    num_loops = 2 * len(group_ranks)
+    # Running Grid in Parallel
+    subprocesses = []
+    ## GRID SEARCH PROJECTIONS
+    for i in group_ranks:
+        subprocesses.append(
+            [
+                "python",
+                f"{model_selector}",
+                f"-n{i}",
+                f"-f{coverage}",
+            ]
+        )
+
+    # Running processes in Parallel
+    # TODO: optimize based on max_workers
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(subprocess.run, cmd) for cmd in subprocesses]
+        # Setting Progress bar to track number of completed subprocesses
+        progress_bar = tqdm(total=num_loops, desc="Progress", unit="subprocess")
+        for future in as_completed(futures):
+            # Update the progress bar for each completed subprocess
+            progress_bar.update(1)
+        progress_bar.close()
