e08e73d70c1d636ba30f46e9638bb63d6ae21e01
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-07-27 16:22:24 +0200

    hypernerve WIP

--- src/jmapping/fitting/__init__.py
+++ src/jmapping/fitting/__init__.py
@@ -1,8 +1,9 @@
-# Sys path update, then call from modeling import *. 
-import os 
-import sys 
+# Sys path update, then call from modeling import *.
+import os
+import sys
 from dotenv import load_dotenv
-load_dotenv() 
+
+load_dotenv()
 src = os.getenv("src")
 sys.path.append(src)
 sys.path.append(src + "jmapping/fitting")
@@ -10,8 +11,6 @@ sys.path.append(src + "jmapping/fitting")
 from fitting.jmapper import JMapper
 from fitting.tupper import Tupper
 from fitting.jgraph import JGraph
+from fitting.nerve import HyperNerve
 from fitting.nammu import *
 from fitting.jmap_helper import *
-
-
-
