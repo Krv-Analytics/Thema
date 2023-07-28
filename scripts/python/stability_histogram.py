76c7b927ae4f6d19e16c8a8994f589ba16a377e4
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-07-28 11:56:10 +0200

    fixing imports for stability histogram

--- scripts/python/stability_histogram.py
+++ scripts/python/stability_histogram.py
@@ -12,9 +12,9 @@ from python_log_indenter import IndentedLoggerAdapter
 load_dotenv()
 src = os.getenv("src")
 sys.path.append(src)
-sys.path.append(src + "jmapping/fitting")
+sys.path.append(src + "jmapping/selecting")
 
-from fitting.jmap_selector_helper import unpack_policy_group_dir, get_viable_jmaps
+from jmapping.selecting import unpack_policy_group_dir, get_viable_jmaps
 from curvature_histogram import plot_curvature_histogram
 
 
