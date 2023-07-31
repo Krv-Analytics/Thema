f61cba207664595ab341ec27fa21cfe0d2dca2b8
Stuart Wayland <stuartwayland@ucsb.edu>
2023-07-31 14:59:58 -0700

    cleaning up bash scripts

--- scripts/bash/jmap_selector.sh
+++ scripts/bash/jmap_selector.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-
-# Load the .env file
-if [ -f ../.env ]; then
-    source ../.env
-fi
-
-# Access Parameter Json file 
-if [ -n "$JSON_PATH" ]; then 
-    params=$(jq -r 'to_entries | .[] | "export \(.key)=\(.value)"' "$JSON_PATH")    eval "$params" 
-fi
-
 echo "Please enter a policy_group size:"
 read NUM_GROUPS
 
