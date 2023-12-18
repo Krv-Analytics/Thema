013410ccbe937c46b996d33bdd100350cbc7eac1
Stuart Wayland <stuartwayland@ucsb.edu>
2023-12-17 19:04:26 -0800

    removing erroneous call to mkdir

--- scripts/bash/cleaner.sh
+++ scripts/bash/cleaner.sh
@@ -2,18 +2,6 @@
 # Cleaning script to drive src/processing/cleaning/cleaner.py
 # Note: Must be run from scripts/ directory! 
 
-
-# Load the .env file
-if [ -f ../../.env ]; then
-    source ../../.env
-fi
-
-
-
-if [ ! -d "${root}/data/clean" ]; then
-    mkdir "${root}/data/clean"
-fi
-
 # Calling cleaning and imputing scripts. Configure options in params.yaml
 poetry run python ../../src/processing/cleaning/cleaner.py  -v  
 poetry run python ../../src/processing/imputing/imputer.py  -v  
