83b8c7e1867f1fd9c173980c34d2495e679952c9
Stuart Wayland <stuartwayland@ucsb.edu>
2023-12-17 19:21:22 -0800

    resolving imports

--- src/processing/imputing/__init__.py
+++ src/processing/imputing/__init__.py
@@ -6,5 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 root = os.getenv("root")
 sys.path.append(root)
+sys.path.append(root + "src/")
 
+import processing
 from scripts.python.utils import env
