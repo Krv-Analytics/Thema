9c3e8aa3ada7daf9603104f481f53385037c5edf
jeremy.wayland <jeremy.don.wayland@gmail.com>
2023-12-19 13:28:16 -0800

    sort policy group log output

--- scripts/python/selector.py
+++ scripts/python/selector.py
@@ -93,6 +93,7 @@ if __name__ == "__main__":
         i = unpack_policy_group_dir(folder)
         group_ranks.append(i)
 
+    group_ranks.sort()
     logging.basicConfig(format="%(message)s", level=logging.INFO)
     log = IndentedLoggerAdapter(logging.getLogger(__name__))
     # LOGGING
