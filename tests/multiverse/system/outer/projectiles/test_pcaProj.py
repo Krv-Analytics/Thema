0bc6b3f0ec63f64b1b0eca09e33087fe9edafbfe
jeremy.wayland <jeremy.don.wayland@gmail.com>
2024-06-02 20:58:07 -0700

    re-activating commented pca tests

--- tests/multiverse/system/outer/projectiles/test_pcaProj.py
+++ tests/multiverse/system/outer/projectiles/test_pcaProj.py
@@ -1,6 +1,6 @@
 # File: tests/multiverse/system/outer/projectiles/test_pcaProj.py
-# Lasted Updated: 04-05-24
-# Updated By: SW
+# Lasted Updated: 06/02/24
+# Updated By: JW
 
 import os
 import pytest
@@ -14,50 +14,48 @@ from thema.multiverse.system.outer.projectiles.pcaProj import pcaProj
 
 
 class Test_pcaProj:
-    """ """
+    def test_init_empty(self):
+        with pytest.raises(TypeError):
+            pcaProj()
 
-    # def test_init_empty(self):
-    #     with pytest.raises(TypeError):
-    #         pcaProj()
+    def test_init(self, tmp_moonAndData):
+        tmp_data, tmp_moon = tmp_moonAndData
+        dimensions = 2
+        seed = 42
+        x = pcaProj(
+            data_path=tmp_data.name,
+            clean_path=tmp_moon.name,
+            dimensions=dimensions,
+            seed=seed,
+        )
 
-    # def test_init(self, tmp_moonAndData):
-    #     tmp_data, tmp_moon = tmp_moonAndData
-    #     dimensions = 2
-    #     seed = 42
-    #     x = pcaProj(
-    #         data_path=tmp_data.name,
-    #         clean_path=tmp_moon.name,
-    #         dimensions=dimensions,
-    #         seed=seed,
-    #     )
+        assert x.get_data_path() == tmp_data.name
+        assert x.get_clean_path() == tmp_moon.name
+        assert_frame_equal(x.data, ut._test_data_0)
+        assert_frame_equal(x.clean, ut._test_cleanData_0)
+        assert x.dimensions == dimensions
+        assert x.seed == seed
 
-    #     assert x.get_data_path() == tmp_data.name
-    #     assert x.get_clean_path() == tmp_moon.name
-    #     assert_frame_equal(x.data, ut._test_data_0)
-    #     assert_frame_equal(x.clean, ut._test_cleanData_0)
-    #     assert x.dimensions == dimensions
-    #     assert x.seed == seed
+    def test_fit(self, tmp_moonAndData):
+        tmp_data, tmp_moon = tmp_moonAndData
+        dimensions = 2
+        seed = 42
+        x = pcaProj(
+            data_path=tmp_data.name,
+            clean_path=tmp_moon.name,
+            dimensions=dimensions,
+            seed=seed,
+        )
+        x.fit()
 
-    # def test_fit(self, tmp_moonAndData):
-    #     tmp_data, tmp_moon = tmp_moonAndData
-    #     dimensions = 2
-    #     seed = 42
-    #     x = pcaProj(
-    #         data_path=tmp_data.name,
-    #         clean_path=tmp_moon.name,
-    #         dimensions=dimensions,
-    #         seed=seed,
-    #     )
-    #     x.fit()
-
-    #     assert x.get_data_path() == tmp_data.name
-    #     assert x.get_clean_path() == tmp_moon.name
-    #     assert_frame_equal(x.data, ut._test_data_0)
-    #     assert_frame_equal(x.clean, ut._test_cleanData_0)
-    #     assert x.dimensions == dimensions
-    #     assert x.seed == seed
-    #     assert x.projectionArray.shape[1] == dimensions
-    #     assert x.projectionArray.shape[0] == x.data.shape[0]
+        assert x.get_data_path() == tmp_data.name
+        assert x.get_clean_path() == tmp_moon.name
+        assert_frame_equal(x.data, ut._test_data_0)
+        assert_frame_equal(x.clean, ut._test_cleanData_0)
+        assert x.dimensions == dimensions
+        assert x.seed == seed
+        assert x.projectionArray.shape[1] == dimensions
+        assert x.projectionArray.shape[0] == x.data.shape[0]
 
     def test_save(self, tmp_moonAndData):
         tmp_data, tmp_moon = tmp_moonAndData
